1cb5fbfcfdd902112191197b3a38accd54a9d19a
ito <i>
2017-07-27 12:12:13 +0900

    ショットリファクタリング

--- Solution/AceLiveMonitor/Shot.cpp
+++ Solution/AceLiveMonitor/Shot.cpp
@@ -3,25 +3,34 @@
 const int SHOT_SPEED = 10; 
 
 Shot::Shot( const Vector& pos, DIR dir ) :
-_dir( dir ),
-_pos( pos ) {
-	_vec = Vector( SHOT_SPEED, 0 );
+Character( pos, NORMAL_CHAR_GRAPH_SIZE, false ) {
+	Vector vec = Vector( SHOT_SPEED, 0 );
 	if ( dir == DIR_LEFT ) {
-		_vec.x *= -1;
+		vec.x *= -1;
 	}
+	setVec( vec );
 }
 
 Shot::~Shot( ) {
 }
 
-Vector Shot::getPos( ) const {
-	return _pos;
+void Shot::act( ) {
 }
 
-void Shot::update( ) {
-	_pos += _vec;
-}
-
-DIR Shot::getDir( ) const {
-	return _dir;
+Chip Shot::getChip( ) const {
+	Vector pos = getPos( );
+	Chip chip = Chip( );
+	chip.tx = 64;
+	chip.ty = 128;
+	chip.size = getChipSize( );
+	chip.sx1 = ( int )pos.x - chip.size / 2;
+	chip.sx2 = chip.sx1 + chip.size;
+	chip.sy1 = ( int )pos.y - chip.size;
+	chip.sy2 = chip.sy1 + chip.size;
+	if ( getDir( ) == DIR_RIGHT ) {
+		int tmp = chip.sx1;
+		chip.sx1 = chip.sx2;
+		chip.sx2 = tmp;
+	}
+	return chip;
 }
