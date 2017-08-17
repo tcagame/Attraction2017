c242718f630dc340a2c159f689f9859543f7d020
ito <i>
2017-08-17 15:05:29 +0900

    hellfire

--- Solution/AceLiveMonitor/EnemyHellFire.cpp
+++ Solution/AceLiveMonitor/EnemyHellFire.cpp
@@ -1,39 +1,37 @@
-#include "EnemyCrocoSnake.h"
+#include "EnemyHellFire.h"
+#include "Family.h"
 
+static const int HP = 3;
+static const double MOVE_SPEED = 3;
+static const double ACCEL = 0.5;
 static const int WAIT_ANIM_TIME = 5;
-static const int MOVE_SPEED = 1;
-static const int MAX_HP = 3;
 
-EnemyCrocoSnake::EnemyCrocoSnake( const Vector& pos ) :
-Enemy( pos, NORMAL_CHAR_GRAPH_SIZE, MAX_HP ),
-_before_pos( Vector( ) ) {
-	setRadius( 30 );
+EnemyHellFire::EnemyHellFire( const Vector& pos ) :
+Enemy( pos, NORMAL_CHAR_GRAPH_SIZE, HP, false ) {
 	setVec( Vector( MOVE_SPEED, 0 ) );
 }
 
 
-EnemyCrocoSnake::~EnemyCrocoSnake( ) {
+EnemyHellFire::~EnemyHellFire( ) {
 }
 
-void EnemyCrocoSnake::act( ) {
-	if ( _before_pos.x == getPos( ).x ) {
-		if ( getDir( ) == DIR_LEFT ) {
-			setVec( Vector( MOVE_SPEED, 0 ) );
-		} else {
-			setVec( Vector( -MOVE_SPEED, 0 ) );
-		}
-	}
-	_before_pos = getPos( );
+void EnemyHellFire::act( ) {
+	Vector target = Vector( Family::getTask( )->getCameraPos( ) + GRAPH_SIZE, GRAPH_SIZE / 2 );
+	Vector distance = target - getPos( );
+	Vector vec = getVec( ) + distance.normalize( ) * ACCEL;
+	vec = vec.normalize( ) * MOVE_SPEED;
+	setVec( vec );
 }
 
-Chip EnemyCrocoSnake::getChip( ) const {
+Chip EnemyHellFire::getChip( ) const {
 	const int ANIM[ ] = {
-		0, 1, 2, 3, 4, 5, 4, 3, 2, 1
+		 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+		10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
 	};
 	int anim_size = sizeof( ANIM ) / sizeof( ANIM[ 0 ] );
 	Chip chip = Chip( );
 	chip.tx = ANIM[ getActCount( ) / WAIT_ANIM_TIME % anim_size ] * 64;
-	chip.ty = 8 * 64;
+	chip.ty = 16 * 64;
 	chip.size = getChipSize( );
 	
 	Vector pos = getPos( );
