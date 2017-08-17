0df47fbab3b446ad137f7b0430e598073ca6e2b1
okuty <a>
2017-08-18 01:46:02 +0900

    同期データ作成

---
+++ Solution/AceCommon/SynchronousData.cpp
@@ -0,0 +1,127 @@
+#include "SynchronousData.h"
+#include <assert.h>
+
+SynchronousData::SynchronousData( ) {
+}
+
+
+SynchronousData::~SynchronousData( ) {
+}
+
+void * SynchronousData::getPtr( ) {
+	return ( void * )&_data;
+}
+
+int SynchronousData::getSize( ) {
+	return sizeof( _data );
+}
+
+int SynchronousData::getStatusPower( int idx ) {
+	assert( idx < ACE_PLAYER_NUM );
+	return _data.status[ idx ].power;
+}
+
+int SynchronousData::getStatusMoney( int idx ) {
+	assert( idx < ACE_PLAYER_NUM );
+	return _data.status[ idx ].money;
+}
+
+bool SynchronousData::isInProssessionOfStatusItem( int idx, unsigned char item ) {
+	assert( idx < ACE_PLAYER_NUM );
+	return ( _data.status[ idx ].items & item) != 0;
+}
+
+int SynchronousData::getStatusVirtue( int idx ) {
+	return _data.status[ idx ].virtue;
+}
+
+int SynchronousData::getStatusRedo( int idx ) {
+	return _data.status[ idx ].redo;
+}
+
+unsigned char SynchronousData::getStatusState( int idx ) {
+	return _data.status[ idx ].state;
+}
+
+void SynchronousData::setStatusPower( int idx, int power ) {
+	assert( idx < ACE_PLAYER_NUM );
+	assert( power >= 0 );
+	_data.status[ idx ].power = ( unsigned char )power;
+}
+
+void SynchronousData::setStatusMoney( int idx, int money ) {
+	assert( idx < ACE_PLAYER_NUM );
+	assert( money >= 0 );
+	_data.status[ idx ].power = ( unsigned long )money;
+}
+
+void SynchronousData::setInProssessionOfStatusItem( int idx, unsigned char item, bool possession ) {
+	assert( idx < ACE_PLAYER_NUM );
+	if ( possession ) {
+		_data.status[ idx ].items |= item;
+	} else {
+		_data.status[ idx ].items &= ~item;
+	}
+}
+
+void SynchronousData::setStatusVirtue( int idx, int virtue ) {
+	assert( idx < ACE_PLAYER_NUM );
+	assert( virtue >= 0 );
+	_data.status[ idx ].power = ( unsigned char )virtue;
+}
+
+void SynchronousData::setStatusRedo( int idx, int redo ) {
+	assert( idx < ACE_PLAYER_NUM );
+	assert( redo >= 0 );
+	_data.status[ idx ].power = ( unsigned char )redo;
+}
+
+void SynchronousData::setStatusState( int idx, unsigned char state ) {
+	assert( idx < ACE_PLAYER_NUM );
+	_data.status[ idx ].power = ( unsigned char )state;
+}
+
+
+void SynchronousData::resetObject( ) {
+	_object_num = 0;
+}
+
+int SynchronousData::getObjectNum( ) {
+	return _object_num;
+}
+
+int SynchronousData::getObjectX( int idx ) {
+	assert( idx < _object_num );
+	return _data.object[ idx ].x;
+}
+
+int SynchronousData::getObjectY( int idx ) {
+	assert( idx < _object_num );
+	return _data.object[ idx ].y;
+}
+
+unsigned char SynchronousData::getObjectType( int idx ) {
+	assert( idx < _object_num );
+	return _data.object[ idx ].type;
+}
+
+int SynchronousData::getObjectPattern( int idx ) {
+	assert( idx < _object_num );
+	return _data.object[ idx ].pattern;
+}
+
+void SynchronousData::addObject( unsigned char type, int pattern, unsigned char attribute, int x, int y ) {
+	if ( _object_num >= OBJECT_NUM ) {
+		return;
+	}
+	assert( x >= 0 );
+	assert( y >= 0 );
+	assert( pattern >= 0 );
+	_data.object[ _object_num ].type      = type;
+	_data.object[ _object_num ].pattern   = ( unsigned char )pattern;
+	_data.object[ _object_num ].attribute = attribute;
+	_data.object[ _object_num ].x         = ( unsigned long )x;
+	_data.object[ _object_num ].y         = ( unsigned long )y;
+	_object_num++;
+}
+
---
+++ Solution/AceCommon/SynchronousData.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "ace_define.h"
+#include "Data.h"
+
+class SynchronousData : public Data {
+public:
+	static const unsigned char ITEM_DANGO       = 0x01;
+	static const unsigned char ITEM_HEART       = 0x02;
+	static const unsigned char ITEM_HYPERTROPHY = 0x04;
+	static const unsigned char ITEM_SHORTENING  = 0x08;
+	static const unsigned char ITEM_WOOD        = 0x10;
+	static const unsigned char ITEM_FLAME       = 0x20;
+	static const unsigned char ITEM_MINERAL     = 0x40;
+public:
+	SynchronousData( );
+	virtual ~SynchronousData( );
+public:
+	void * getPtr( );
+	int getSize( );
+	int getStatusPower( int idx );
+	int getStatusMoney( int idx );
+	bool isInProssessionOfStatusItem( int idx, unsigned char item );
+	int getStatusVirtue( int idx );
+	int getStatusRedo( int idx );
+	unsigned char getStatusState( int idx );
+	void setStatusPower( int idx, int power );
+	void setStatusMoney( int idx, int money );
+	void setInProssessionOfStatusItem( int idx, unsigned char item, bool possession );
+	void setStatusVirtue( int idx, int virtue );
+	void setStatusRedo( int idx, int redo );
+	void setStatusState( int idx, unsigned char state );
+	void resetObject( );
+	int getObjectNum( );
+	int getObjectX( int idx );
+	int getObjectY( int idx );
+	unsigned char getObjectType( int idx );
+	int getObjectPattern( int idx );
+	void addObject( unsigned char type, int pattern, unsigned char attribute, int x, int y );
+private:
+	static const int OBJECT_NUM = 140;
+
+	#pragma pack( 1 )
+		struct SyncData {
+			struct Status {
+				unsigned char power;
+				unsigned char money;
+				unsigned char items;
+				unsigned char virtue;
+				unsigned char redo;
+				unsigned char state;
+			} status[ ACE_PLAYER_NUM ];
+			struct Object {
+				unsigned char type;
+				unsigned char pattern;
+				unsigned char attribute;
+				unsigned long x;
+				unsigned long y;
+			} object[ OBJECT_NUM ];
+		};
+	#pragma pack( )	
+private:
+	SyncData _data;
+	int _object_num;
+};
+
