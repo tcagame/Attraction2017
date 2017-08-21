f1ab935311229f6f7dca8d085e6d5472697c4705
kase <kytk>
2017-08-21 11:08:16 +0900

    青坊主移行

--- Solution/AceLiveMonitor/EnemyBlueMonk.cpp
+++ Solution/AceLiveMonitor/EnemyBlueMonk.cpp
@@ -1,4 +1,5 @@
 #include "EnemyBlueMonk.h"
+#include "SynchronousData.h"
 
 static const int WAIT_ANIM_TIME = 5;
 static const int MOVE_SPEED = 2;
@@ -23,28 +24,29 @@ void EnemyBlueMonk::act( ) {
 	_before_pos = getPos( );
 }
 
-Chip EnemyBlueMonk::getChip( ) const {
+void EnemyBlueMonk::setSynchronousData( unsigned char type, int camera_pos ) const {
 	const int ANIM[ ] = {
-		7, 8, 9, 10, 11, 12
+		187, 188, 189, 190, 191, 192
 	};
 	int anim_size = sizeof( ANIM ) / sizeof( ANIM[ 0 ] );
-	Chip chip = Chip( );
-	chip.tx = ANIM[ getActCount( ) / WAIT_ANIM_TIME % anim_size ] * 64;
-	chip.ty = 9 * 64;
-	chip.size = getChipSize( );
 	
 	Vector pos = getPos( );
-	DIR dir = getDir( );
-	if ( dir == DIR_RIGHT ){
-		chip.sx1 = ( int )pos.x - chip.size / 2 + chip.size;
-		chip.sy1 = ( int )pos.y - chip.size;
-		chip.sx2 = chip.sx1 - chip.size;
-		chip.sy2 = chip.sy1 + chip.size;
-	} else {
-		chip.sx1 = ( int )pos.x - chip.size / 2;
-		chip.sy1 = ( int )pos.y - chip.size;
-		chip.sx2 = chip.sx1 + chip.size;
-		chip.sy2 = chip.sy1 + chip.size;
+	int x = ( int )pos.x;
+	int y = ( int )pos.y;
+
+	AREA area = AREA_EVENT;
+	if ( getState( ) == STATE_MAIN ) {
+		x -= camera_pos;
+		area = AREA_MAIN;
 	}
-	return chip;
+	unsigned char attribute = 0;
+	if ( getDir( ) == DIR_RIGHT ) {
+		attribute |= SynchronousData::ATTRIBUTE_REVERSE;
+	}
+	SynchronousDataPtr data( SynchronousData::getTask( ) );
+	data->addObject( area, type, ANIM[ getActCount( ) / WAIT_ANIM_TIME % anim_size ], attribute, x, y );
+}
+
+Chip EnemyBlueMonk::getChip( ) const {
+	return Chip( );
 }
