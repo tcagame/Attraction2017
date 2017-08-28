705177e91d9481cefb8325c96e002fc257d6a965
kase <kasu>
2017-08-28 16:08:01 +0900

    add:shobon

--- Solution/RockClientTest/RockMapTest.cpp
+++ Solution/RockClientTest/RockMapTest.cpp
@@ -29,7 +29,7 @@ RockMapTest::RockMapTest( ) {
 	
 	RockStoragePtr sorage = RockStorage::getTask();
 	sorage->addAlter( RockAlterPtr( new RockAlter( Vector( 200, 0, 0 ) ) ) );
-	sorage->addCasket( RockCasketPtr( new RockCasket( Vector( 0, 0, 0 ) ) ) );
+	sorage->addCasket( RockCasketPtr( new RockCasket( Vector( 0, 0, -50 ) ) ) );
 
 }
 
