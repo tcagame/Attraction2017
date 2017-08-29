ba5547889f2b8e8e95bb01e3e100a612f96c1146
yamada <a>
2017-08-29 15:39:14 +0900

    泡

--- Solution/RockClientTest/RockMilitaryTest.cpp
+++ Solution/RockClientTest/RockMilitaryTest.cpp
@@ -13,7 +13,7 @@ RockMilitaryTest::RockMilitaryTest( ) {
 	//add( RockEnemyPtr( new RockEnemyRedBard( Vector( 10, 30, 10 ) ) ) );
 	//add( RockEnemyPtr( new RockEnemyWaterGhost( Vector( -10, 30, 30 ) ) ) );
 	//add( RockEnemyPtr( new RockEnemyBossRock( Vector( -50, 50, 50 ) ) ) );
-	add( RockEnemyPtr( new RockEnemyFaceAndHand( Vector( -50, 50, 50 ) ) ) );
+	//add( RockEnemyPtr( new RockEnemyFaceAndHand( Vector( -50, 50, 50 ) ) ) );
 	//add( RockEnemyPtr( new RockEnemyCloud( Vector( -50, 50, 50 ) ) ) );
 }
 
