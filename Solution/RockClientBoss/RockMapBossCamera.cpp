452d02e592ac77f2319c6cd50827948e7e97f8f5
yamada <a>
2017-08-31 14:22:21 +0900

    カメラ位置調整

--- Solution/RockClientBoss/RockMapBossCamera.cpp
+++ Solution/RockClientBoss/RockMapBossCamera.cpp
@@ -3,8 +3,8 @@
 #include "Drawer.h"
 #include "RockFamily.h"
 
-const Vector INIT_POS = Vector( -1500, 1000, 1000 );
-const Vector INIT_TARGET = Vector( 0, 60, 0 );
+const Vector INIT_POS = Vector( 500, 500, -800 );
+const Vector INIT_TARGET = Vector( 500, 60, 0 );
 const Vector DIR = Vector( 0, 0.8, -1 ).normalize( );
 const double LENGTH = 300;
 
