a9364ac8721970b862fa709230a270c541ca1e9b
ito <i>
2017-09-05 11:12:22 +0900

    enemy修正　亡者ボイス追加

--- Solution/AceLiveMonitor/EnemySwampZombieB.cpp
+++ Solution/AceLiveMonitor/EnemySwampZombieB.cpp
@@ -1,5 +1,6 @@
 #include "EnemySwampZombieB.h"
 #include "SynchronousData.h"
+#include "Sound.h"
 
 static const int WAIT_ANIM_TIME = 5;
 static const int MAX_HP = 3;
@@ -7,6 +8,10 @@ static const int MAX_HP = 3;
 EnemySwampZombieB::EnemySwampZombieB( const Vector& pos ) :
 Enemy( pos, NORMAL_CHAR_GRAPH_SIZE, MAX_HP ) {
 	setRadius( 36 );
+	SoundPtr sound = Sound::getTask( );
+	if ( !sound->isPlayingSE( "yokai_voice_04.wav" ) ) {
+		sound->getTask( )->playSE( "yokai_voice_04.wav" );
+	}
 }
 
 
@@ -19,7 +24,7 @@ void EnemySwampZombieB::act( ) {
 
 void EnemySwampZombieB::setSynchronousData( unsigned char type, int camera_pos ) const {
 	const int ANIM[ ] = {
-		100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113
+		100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115
 	};
 	int anim_size = sizeof( ANIM ) / sizeof( ANIM[ 0 ] );
 	
