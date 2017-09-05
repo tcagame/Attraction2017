e57e8673d27b7857cc9c85238683a9e91cc92827
kase <kasu>
2017-09-05 09:46:20 +0900

    rock 背景配置

--- Solution/RockCommon/RockMap.h
+++ Solution/RockCommon/RockMap.h
@@ -16,11 +16,14 @@ public:
 public:
 	std::vector< ModelMV1Ptr > getModels( ) const;
 	std::vector< ModelMV1Ptr > getColModels( ) const;
+	std::vector< ModelMV1Ptr > getBackGroundModels( ) const;
 	void addModel( ModelMV1Ptr model );
 	void addColModel( ModelMV1Ptr model );
+	void addBackGroundModel( ModelMV1Ptr model );
 	void resetModels( );
 private:
 	std::vector< ModelMV1Ptr > _models;
 	std::vector< ModelMV1Ptr > _col_models;
+	std::vector< ModelMV1Ptr > _back_ground_models;
 };
 
