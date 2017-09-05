3f17c040c109751f83d89c7f5339a92faac4ea65
yamad <i>
2017-09-05 14:36:44 +0900

    死亡音追加

--- Solution/AceLiveMonitor/Player.cpp
+++ Solution/AceLiveMonitor/Player.cpp
@@ -114,7 +114,17 @@ void Player::act( ) {
 		}
 	}
 	if ( data->getStatusPower( _player ) == 0 ) {
-		sound->stopSE( "yokai_se_02.wav" );
+		bool stop = true;
+		for ( int i = 0; i < MAX_PLAYER; i++ ) {
+			if ( data->getStatusPower( ( PLAYER ) i ) <= 4 && 
+				 _player != i &&
+				 data->getStatusPower( ( PLAYER ) i ) > 0 ) {
+				stop = false;
+			}
+		}
+		if ( stop ) {
+			sound->stopSE( "yokai_se_02.wav" );
+		}
 	}
 }
 
