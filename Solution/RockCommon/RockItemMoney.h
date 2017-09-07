0ce93427055a7afbf8668beccd6329c39c4eb8f2
iwa <iwa>
2017-09-07 15:05:06 +0900

    着物の妖怪を倒したらガマ口が落ちるようにした

--- Solution/RockCommon/RockItemMoney.h
+++ Solution/RockCommon/RockItemMoney.h
@@ -7,6 +7,8 @@ public:
 	static const int MONEY_VALUE_2 = 500;
 	static const int MONEY_VALUE_3 = 1000;
 	static const int MONEY_VALUE_4 = 5000;
+	static const int MONEY_VALUE_5 = 10000;
+
 public:
 	RockItemMoney( const Vector& pos, int value );
 	virtual ~RockItemMoney( );
