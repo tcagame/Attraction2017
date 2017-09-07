738399673691cebd65ca570e93899f748875c800
ito <i>
2017-09-07 12:04:55 +0900

    影追加

--- Solution/RockClientTest/main.cpp
+++ Solution/RockClientTest/main.cpp
@@ -20,6 +20,7 @@
 #include "RockStorage.h"
 #include "RockTheaterTest.h"
 #include "RockStudio.h"
+#include "RockShadow.h"
 
 void main( ) {
 
@@ -39,6 +40,7 @@ void main( ) {
 	app->addTask( RockCamera::getTag( ), TaskPtr( new RockCameraTest ) );
 	app->addTask( MessageSender::getTag( ), TaskPtr( new MessageSender( message ) ) );
 	//app->addTask( RockTheater::getTag( ), TaskPtr( new RockTheaterTest( ) ) );
+	app->addTask( RockShadow::getTag( ), TaskPtr( new RockShadow( ) ) );
 	app->addTask( RockStudio::getTag( ), TaskPtr( new RockStudio( ) ) );
 
 	std::vector< unsigned int > state = { };
