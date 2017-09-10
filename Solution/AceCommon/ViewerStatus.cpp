db22073f20fe93375b99874c7f8e912a82676a98
okuty <a>
2017-09-10 15:03:39 +0900

    Ace ステータスリソース差し替え中

--- Solution/AceCommon/ViewerStatus.cpp
+++ Solution/AceCommon/ViewerStatus.cpp
@@ -6,17 +6,22 @@
 
 const int HP_GRAPH_HEIGHT = 16;
 const int HP_GRAPH_WIDTH = 8;
-const int BUSTUP_OX = 10;
+const int BUSTUP_OX = 20;
 const int BUSTUP_OY = 10;
+const int NAME_OX = 145;
+const int NAME_OY = 5;
 
 ViewerStatus::ViewerStatus( ) {
 	DrawerPtr drawer( Drawer::getTask( ) );
 	_image_frame = drawer->createImage( "UI/status_plate.png" );
-	_status_ui = drawer->createImage( "UI/ui.png" );
 	_image_bustup[ PLAYER_TAROSUKE ] = drawer->createImage( "UI/ui_bustup_tarosuke.png" );
 	_image_bustup[ PLAYER_TAROJIRO ] = drawer->createImage( "UI/ui_bustup_tarojiro.png" );
 	_image_bustup[ PLAYER_GARISUKE ] = drawer->createImage( "UI/ui_bustup_garisuke.png" );
 	_image_bustup[ PLAYER_TAROMI   ] = drawer->createImage( "UI/ui_bustup_taromi.png"   );
+	_image_name[ PLAYER_TAROSUKE ] = drawer->createImage( "UI/ui_name_tarosuke.png" );
+	_image_name[ PLAYER_TAROJIRO ] = drawer->createImage( "UI/ui_name_tarojiro.png" );
+	_image_name[ PLAYER_GARISUKE ] = drawer->createImage( "UI/ui_name_garisuke.png" );
+	_image_name[ PLAYER_TAROMI   ] = drawer->createImage( "UI/ui_name_taromi.png"   );
 }
 
 ViewerStatus::~ViewerStatus( ) {
@@ -29,30 +34,14 @@ void ViewerStatus::draw( PLAYER player, int sx, int sy ) const {
 	_image_frame->setPos( sx, sy );
 	_image_frame->draw( );
 	drawBustup( player, sx, sy );
-	drawPower(  data->getStatusPower( player ), sx, sy );
-	drawMoney(  data->getStatusMoney( player ), sx, sy );
 	drawVirtue( data->getStatusVirtue( player ), sx, sy );
 }
 
 void ViewerStatus::drawBustup( PLAYER player, int sx, int sy ) const {
 	_image_bustup[ player ]->setPos( sx + BUSTUP_OX, sy + BUSTUP_OY );
 	_image_bustup[ player ]->draw( );
-}
-
-void ViewerStatus::drawPower( int power, int sx, int sy ) const {
-	int tw = HP_GRAPH_WIDTH * power;
-	_status_ui->setRect( 0, 256, tw, HP_GRAPH_HEIGHT );
-	sx += 160;
-	sy +=  50;
-	_status_ui->setPos( sx, sy );
-	_status_ui->draw( );
-}
-
-void ViewerStatus::drawMoney( int money,  int sx, int sy ) const {
-	sx += 160;
-	sy += 105;
-	DrawerPtr drawer( Drawer::getTask( ) );
-	drawer->drawString( sx, sy, "%15d", money );
+	_image_name[ player ]->setPos( sx + NAME_OX, sy + NAME_OY );
+	_image_name[ player ]->draw( );
 }
 
 void ViewerStatus::drawVirtue( int virtue, int sx, int sy ) const {
--- Solution/AceCommon/ViewerStatus.h
+++ Solution/AceCommon/ViewerStatus.h
@@ -12,12 +12,10 @@ public:
 	void draw( PLAYER player, int sx, int sy ) const;
 private:
 	void drawBustup( PLAYER player, int sx, int sy ) const;
-	void drawPower( int power, int sx, int sy ) const;
-	void drawMoney( int money, int sx, int sy ) const;
 	void drawVirtue( int virtue, int sx, int sy ) const;
 private:
 	ImagePtr _image_frame;
-	ImagePtr _status_ui;
 	std::array< ImagePtr, MAX_PLAYER > _image_bustup;
+	std::array< ImagePtr, MAX_PLAYER > _image_name;
 };
 
