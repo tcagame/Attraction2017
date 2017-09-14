6ad65dca9fc95b66a5a60110e0c166e9d093470b
ito <i>
2017-09-14 10:38:51 +0900

    risultステージ追加

--- Solution/RockClientResult/RockCameraResult.cpp
+++ Solution/RockClientResult/RockCameraResult.cpp
@@ -1,8 +1,12 @@
 #include "RockCameraResult.h"
 #include "Application.h"
 #include "Drawer.h"
+#include "RockFamily.h"
 
-const Vector INIT_POS = Vector( 0, 120, -270 );
+const Vector INIT_POS = Vector( 200, 80, -180 );
+const Vector DIR = Vector( 0, 0.5, -1 ).normalize( );
+const double LENGTH = 300;
+
 
 RockCameraResult::RockCameraResult( ) :
 RockCamera( INIT_POS, Vector( ) ){
@@ -14,4 +18,13 @@ RockCameraResult::~RockCameraResult( ) {
 
 
 void RockCameraResult::setCamera( ) {
+	Vector target = RockFamily::getTask( )->getCameraPos( );
+	if ( target.isOrijin( ) ) {
+		setPos( INIT_POS );
+		setTarget( Vector( ) );
+	} else {
+		Vector pos = target + DIR * LENGTH;
+		setPos( pos );
+		setTarget( target );
+	}
 }
--- Solution/RockClientResult/RockMapResult.cpp
+++ Solution/RockClientResult/RockMapResult.cpp
@@ -10,14 +10,34 @@ RockMapResult::~RockMapResult( ) {
 
 void RockMapResult::initialize( ) {
 	ModelMV1Ptr map01 = ModelMV1Ptr( new ModelMV1 );
-	map01->load( "Resource/Rock/map/test/map01.mv1" );
+	map01->load( "Resource/Rock/map/result/map11_01.mv1" );
 	addModel( map01 );
 	
 	ModelMV1Ptr col01 = ModelMV1Ptr( new ModelMV1 );
-	col01->load( "Resource/Rock/map/test/map01_col.mv1" );
+	col01->load( "Resource/Rock/map/result/map11_01.mv1" );
 	col01->setTrans( Matrix::makeTransformTranslation( Vector( 0, 10, 0 ) ) );
 	col01->draw( );
 	addColModel( col01 );
+
+	//”wŒi
+	ModelMV1Ptr map03 = ModelMV1Ptr( new ModelMV1 );
+	map03->load( "Resource/Rock/map/result/map11_03.mv1" );
+	addModel( map03 );
+
+	ModelMV1Ptr map05 = ModelMV1Ptr( new ModelMV1 );
+	map05->load( "Resource/Rock/map/result/map11_05.mv1" );
+	addModel( map05 );
+
+	ModelMV1Ptr map04 = ModelMV1Ptr( new ModelMV1 );
+	map04->load( "Resource/Rock/map/result/map11_04.mv1" );
+	addModel( map04 );
+
+	ModelMV1Ptr map02 = ModelMV1Ptr( new ModelMV1 );
+	map02->load( "Resource/Rock/map/result/map11_02.mv1" );
+	addModel( map02 );
+
+	
+
 }
 
 
