a69ef5f6d2b7ab4da1160309ec0e758eab7cab87
kase <kasu>
2017-09-19 17:45:57 +0900

    赤鬼が子赤鬼生成

--- Solution/RockCommon/RockEnemyBossRedDaemon.cpp
+++ Solution/RockCommon/RockEnemyBossRedDaemon.cpp
@@ -4,12 +4,18 @@
 #include "RockFamily.h"
 #include "RockPlayer.h"
 #include "RockDollHouse.h"
+#include "RockMilitary.h"
+#include "RockEnemyLittleRedDaemon.h"
 
 const int HP = 1;
-static const double ANIM_SPEED = 0.9;
+const double ANIM_SPEED = 0.9;
+const int GENERATE_NUM = 4;
+const int GENERATE_TIME = 300;
+const int POP_NUM = 4;
 
 RockEnemyBossRedDaemon::RockEnemyBossRedDaemon( const Vector& pos ) :
-RockEnemyBoss( pos, DOLL_BOSS_RED_DAEMON, HP, 1, 10, 10, true, true ) {
+RockEnemyBoss( pos, DOLL_BOSS_RED_DAEMON, HP, 1, 10, 10, true, true ),
+_count( 0 ) {
 }
 
 
@@ -17,7 +23,21 @@ RockEnemyBossRedDaemon::~RockEnemyBossRedDaemon( ) {
 }
 
 void RockEnemyBossRedDaemon::act( ) {
-	return;
+	Vector generate_pos( getPos( ) );
+	const Vector GENERATE_POS[ GENERATE_NUM ] = { 
+		Vector( generate_pos.x + 30, generate_pos.y, generate_pos.z ),
+		Vector( generate_pos.x - 30, generate_pos.y, generate_pos.z ),
+		Vector( generate_pos.x, generate_pos.y, generate_pos.z + 30 ),
+		Vector( generate_pos.x, generate_pos.y, generate_pos.z - 30 ),
+	};
+
+	RockMilitaryPtr military = RockMilitary::getTask( );
+	if ( getActCount( ) % GENERATE_TIME == 0 && _count < POP_NUM ) {
+		for ( int i = 0; i < GENERATE_NUM; i++ ) {
+			military->addEnemy( RockEnemyPtr( new RockEnemyLittleRedDaemon( GENERATE_POS[ i ] ) ) );
+		}
+		_count++;
+	}
 }
 
 double RockEnemyBossRedDaemon::getAnimTime( ) const {
