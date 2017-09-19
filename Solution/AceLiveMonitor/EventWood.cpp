46aa6f58a7945a10c7ab3efc915f98ae5fdb2b36
yamada <a>
2017-09-19 14:46:21 +0900

    csv更新

--- Solution/AceLiveMonitor/EventWood.cpp
+++ Solution/AceLiveMonitor/EventWood.cpp
@@ -1,6 +1,8 @@
 #include "EventWood.h"
 #include "Military.h"
 #include "EnemyBossMonsterTree.h"
+#include "Family.h"
+#include "Player.h"
 
 EventWood::EventWood( ) :
 Event( EVENT_WOOD ) {
@@ -13,16 +15,26 @@ EventWood::~EventWood( ) {
 }
 
 void EventWood::update( ) {
-}
+	// 透明化し切ったら、イベントへ
+	for ( int i = 0; i < MAX_PLAYER; i++ ) {
+		PlayerPtr player = Family::getTask( )->getPlayer( i );
+		if ( player->isEntering( ) ) {
+			player->enterEvent( );
+		}
+	}
 
-bool EventWood::isFinished( ) const {
-	return _boss->getPower( ) <= 0;
+	// ボスを倒した
+	if ( _boss->getPower( ) <= 0 ) {
+		_boss->dropItem( );//神器(木)を配る
+		exit( );
+	}
 }
 
 bool EventWood::isJoining( ) const {
-	return true;
+	return getFade( ) != FADE_OUT;
 }
 
 void EventWood::join( PLAYER target ) {
-
+	PlayerPtr player = Family::getTask( )->getPlayer( target );
+	player->setActionEnteringFadeOut( );
 }
