653a05eadad64efe6de567d14d58fa1544814696
kase <kase*kase>
2017-09-23 21:48:44 +0900

    エネミーのお金おとすふえた

--- Solution/RockCommon/RockEnemyCloud.cpp
+++ Solution/RockCommon/RockEnemyCloud.cpp
@@ -56,5 +56,5 @@ double RockEnemyCloud::getAnimTime( ) const {
 }
 
 void RockEnemyCloud::dropItem( ) {
-	RockStorage::getTask( )->addDropItem( RockItemPtr( new RockItemMoney( getPos( ) + Vector( 0, getOverlappedRadius( ), 0 ), RockItemMoney::MONEY_VALUE_3 ) ) );
+	RockStorage::getTask( )->addDropItem( RockItemPtr( new RockItemMoney( getPos( ) + Vector( 0, getOverlappedRadius( ), 0 ), RockItemMoney::MONEY_VALUE_4 ) ) );
 }
